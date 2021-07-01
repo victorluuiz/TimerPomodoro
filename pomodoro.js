3a9eea79087abec787b4eaea528f2b81f4c890c2
victorluuiz <victorluuiz30@gmail.com>
2021-06-30 21:41:22 -0400

    Declaração das variáveis hh, mm, ss, alt, tempo, temp e das funções start, pause, stop e timer. Botão 'iniciar' será desabilitado quando o temporizador começar a funcionar.

--- pomodoro.js
+++ pomodoro.js
@@ -1,2 +1,62 @@
 "use strict"
 
+var hh = 0;
+var mm = 25;
+var ss = 0;
+var alt = 1;
+var tempo = 1000;
+var temp;
+
+//Inicia o temporizador
+function start() {
+    temp = setInterval(() => { timer(); }, tempo);
+    document.getElementById("iniciar").disabled = true;
+
+}
+
+
+function pause() {
+    clearInterval(temp);
+    if (document.getElementById("iniciar").disabled = true) {
+        document.getElementById("iniciar").disabled = false;
+    }
+}
+
+function stop() {
+    clearInterval(temp);
+    hh = 0;
+    mm = 25;
+    ss = 1;
+
+    document.getElementById('counter').innerText = '00:25:00';
+    if (document.getElementById("iniciar").disabled = true) {
+        document.getElementById("iniciar").disabled = false;
+    }
+}
+
+//Faz a contagem do tempo e exibição
+function timer() {
+
+    if ((mm > 0) || (ss > 0)) {
+        if (ss == 0) {
+            ss = 59;
+            mm--;
+        }
+        else {
+            ss--;
+        }
+        if (mm == 0 && ss == 0) {
+            alt = 0;
+        }
+    }
+    if (alt == 0) {
+        mm == 25;
+        if (ss == 0) {
+            ss = 59; bn
+            mm--;
+        }
+        else {
+            ss--;
+        }
+    }
+}
