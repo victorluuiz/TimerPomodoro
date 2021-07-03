6288f7abc09a8e7af488279f9b0b195ca0041d7a
victorluuiz <victorluuiz30@gmail.com>
2021-07-02 21:35:36 -0400

    Declaração de duas variáveis, que contém arquivos com os sons de notificação

--- pomodoro.js
+++ pomodoro.js
@@ -6,8 +6,8 @@ var ss = 0;
 var alt = 1;
 var tempo = 1000;
 var temp;
-
-
+var notification = new Audio('https://d9olupt5igjta.cloudfront.net/samples/sample_files/9040/e2b7b882993ddcd25771fc9cedac07ca8635f603/mp3/72127-sweetalertsound3-wav.mp3?1512767707');
+var notification2 = new Audio('https://d9olupt5igjta.cloudfront.net/samples/sample_files/71017/6002484fa6ed0e3f78e984542caecdd0a4b3c6b6/mp3/_Discord_Message.mp3?1619010662');
 //0 --> Tarefa
 //1 --> Pausa
 var tarefa = 0;
